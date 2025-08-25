63bc2cd6c9004d4638e6cc8bb14dae14363037ec
f3 <aussiefishsticks@gmail.com>
2025-08-25 16:40:53 -0400

    Update downloader.js

--- scripts/downloader.js
+++ scripts/downloader.js
@@ -107,6 +107,8 @@ window.startdownload = function(box) {  startpreview(box);
     box.downloading = false;
     box.classList.remove("downloading");
     log_to_server("fail " + box.sid);
-      }
+            clearInterval(fadeOut);
+    }
   }, 30);
 };
+
